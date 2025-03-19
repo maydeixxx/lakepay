0e7cd20a84527e167f2ce07429957aad62dc19a9
Alex Amaev <kruki999@yandex.ru>
2025-03-19 21:35:40 +0700

    Start commit

---
+++ src/test/java/com/LakePayProj/LakePay/LakePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LakePayProj.LakePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
