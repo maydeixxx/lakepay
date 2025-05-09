d1973be84d6ad6a4251ade2ea0c165ae47343eea
M.Zinakov <mihailzinakovvv@gmail.com>
2025-05-09 14:43:42 +0700

    Properly configure Mapstruct processor

--- chatService/src/main/java/com/lakepayProj/chatService/mappers/IChatMessageMapper.java
+++ chatService/src/main/java/com/lakepayProj/chatService/mappers/IChatMessageMapper.java
@@ -7,9 +7,9 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IChatMessageMapper {
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "status", ignore = true)
-    @Mapping(target = "chatId", ignore = true)
-    @Mapping(target = "chat", ignore = true)
+    @Mapping(target = "content", source = "content")
+    @Mapping(target = "senderName", source = "senderName")
+    @Mapping(target = "recipientName", source = "recipientName")
+    @Mapping(target = "date", source = "date")
     ChatMessageDTO toDTO(ChatMessage message);
 }
