91b0df00e73ccf2dcbd89d3fc2a687bd17fb5998
M.Zinakov <mihailzinakovvv@gmail.com>
2025-06-18 00:22:39 +0700

    UserDetailsService throws UserNotFoundException if user isn't in db

--- userService/src/main/java/com/LakePayProj/userService/service/UserDetailsServiceImpl.java
+++ userService/src/main/java/com/LakePayProj/userService/service/UserDetailsServiceImpl.java
@@ -1,11 +1,11 @@
 package com.LakePayProj.userService.service;
 
+import com.LakePayProj.userService.exception.UserNotFoundException;
 import com.LakePayProj.userService.security.UserDetailsImpl;
 import com.LakePayProj.userService.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +14,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final IUserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UserNotFoundException {
         return userRepository.findByUsername(username)
                 .map(UserDetailsImpl::new)
-                .orElseThrow(() -> new UsernameNotFoundException("Can't find user with username: " + username));
+                .orElseThrow(() -> new UserNotFoundException("Can't find user with username: " + username));
     }
 }
