7adfeb3f7b6c773adf4f007ab0f7d961f4efcb15
M.Zinakov <mihailzinakovvv@gmail.com>
2025-06-21 01:01:21 +0700

    Added fullname to user DTOs

--- userService/src/main/java/com/LakePayProj/userService/dto/PublicUserDto.java
+++ userService/src/main/java/com/LakePayProj/userService/dto/PublicUserDto.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 public record PublicUserDto(
         String username,
+        String fullName,
         String avatarUrl,
         UserRole role,
         LocalDateTime creationDate
--- userService/src/main/java/com/LakePayProj/userService/dto/UserDto.java
+++ userService/src/main/java/com/LakePayProj/userService/dto/UserDto.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 public record UserDto(
         Long id,
         Long telegramId,
+        String fullName,
         String username,
         String avatarUrl,
         UserRole role,
